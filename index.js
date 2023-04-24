6dc44b0b88366d841650a9258e7d7010802cfedc
honeymirara <honeymirara@gmail.com>
2023-04-24 11:52:32 +0300

    createDatafunction

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { getAll, getDataById, createData } = require('./service/service.js');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    try {
+        const data = getAll();
+        res.send(data);
+    }
+    catch (err) {
+        res.send(err.message);
+    }
+});
+
+app.get('/:id', (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = getDataById(id);
+        res.send(data);
+    } catch (err) {
+        res.send(err.message);
+    }
+
+
+})
+
+app.post('/', (req, res) => {
+    try {
+        const {
+            label, category, priority
+        } = req.body;
+      const createData = createData(label, category, priority)
+    } catch (err) {
+        res.send(err.message);
+    }
+})
+
+app.listen(3000, () => {
+    console.log('server is running');
+})
